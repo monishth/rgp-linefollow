0a01cfdb823545da275fdac883be4480b8d851b5
Monish Thirukkumaran <mish311111@gmail.com>
2019-11-29 12:10:46 +0000

    objectified the robot code

--- src/tbt/LineFollowBot.java
+++ src/tbt/LineFollowBot.java
@@ -11,12 +11,11 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.SensorMode;
-import lejos.robotics.Color;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
-public class LineFollow {
+public class LineFollowBot {
 
     public static final float BLACK = 0.07f;
     public static final float MID = 0.15f;
@@ -24,52 +23,59 @@ public class LineFollow {
     private static float kp = 1700f;
     private static float kd = 0f;
     private static float ki = 0f;
-    private static RegulatedMotor motorRight;
-    private static RegulatedMotor motorLeft;
-    private static RegulatedMotor ultrasoundMotor;
-    private static SensorMode colorSensor;
-    private static SampleProvider ultrasoundSensor;
-    private static float[] sample;
-    private static float[] ultrasoundSample;
+    private RegulatedMotor motorRight;
+    private RegulatedMotor motorLeft;
+    private RegulatedMotor ultrasoundMotor;
+    private SensorMode colorSensor;
+    private SampleProvider ultrasoundSensor;
+    private float[] colorSample;
+    private float[] ultrasoundSample;
+    private int sw;
+    private int sh;
+    private GraphicsLCD g;
 
-    public static void main(String[] args) {
+    public LineFollowBot() {
         motorRight = new EV3LargeRegulatedMotor(MotorPort.A);
         motorLeft = new EV3LargeRegulatedMotor(MotorPort.B);
         ultrasoundMotor = new EV3MediumRegulatedMotor(MotorPort.C);
         colorSensor = new EV3ColorSensor(SensorPort.S1).getRedMode();
         ultrasoundSensor = new EV3UltrasonicSensor(SensorPort.S2).getDistanceMode();
-        //motorLeft.synchronizeWith(new RegulatedMotor[]{motorRight});
-        GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
-        final int SW =  g.getWidth();
-        final int SH = g.getHeight();
-        /*colorSensor.setCurrentMode("Red");
-        colorSensor.setFloodlight(Color.RED);
-        colorSensor.setFloodlight(true);*/
-        sample = new float[colorSensor.sampleSize()];
+        g = BrickFinder.getDefault().getGraphicsLCD();
+        sw = g.getWidth();
+        sh = g.getHeight();
+        colorSample = new float[colorSensor.sampleSize()];
         ultrasoundSample = new float[ultrasoundSensor.sampleSize()];
-        colorSensor.fetchSample(sample, 0);
+        colorSensor.fetchSample(colorSample, 0);
         ultrasoundSensor.fetchSample(ultrasoundSample, 0);
+    }
+
+    public static void main(String[] args) {
+        LineFollowBot robot = new LineFollowBot();
+        robot.lineFollow();
+    }
+
+    public void lineFollow() {
 
         float derivative = 0f;
         float integral = 0f;
         float previousError = 0f;
 
 
-        while (true) {
-            while(ultrasoundSample[0] > 0.1f){
+        while (!Button.LEFT.isDown()) {
+            if (ultrasoundSample[0] > 0.1f) {
                 g.clear();
 
-                colorSensor.fetchSample(sample, 0);
+                colorSensor.fetchSample(colorSample, 0);
                 ultrasoundSensor.fetchSample(ultrasoundSample, 0);
-                float lightLevel = sample[0];
+                float lightLevel = colorSample[0];
                 if (lightLevel < 0.4) {
                     float ultrasoundDistance = ultrasoundSample[0];
-                    g.drawString(lightLevel +" : " + ultrasoundDistance + " m ", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
+                    g.drawString(lightLevel + " : " + ultrasoundDistance + " m ", sw / 2, sh / 2, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
                     g.refresh();
 
                     float error = MID - lightLevel;
 
-                    if(Math.abs(error) < 0.005f){//zero the integral windup
+                    if (Math.abs(error) < 0.005f) {//zero the integral windup
                         integral = 0;
                     }
                     integral = (integral * (2f / 3f)) + error; //dampen the integral
@@ -78,16 +84,16 @@ public class LineFollow {
                     pidSpeed(error, derivative, integral, kp, kd, ki);
                 }
 
-                Delay.msDelay(10);
-
+            } else {
+                avoidObstacle();
             }
 
-            avoidObstacle();
-        }
+            Delay.msDelay(10);
 
+        }
     }
 
-    private static void avoidObstacle() {
+    private void avoidObstacle() {
 
         Sound.twoBeeps();
         turnRight();
@@ -96,29 +102,29 @@ public class LineFollow {
         float integral = 0f;
         float previousError = 0f;
 
-        colorSensor.fetchSample(sample, 0);
+        colorSensor.fetchSample(colorSample, 0);
         ultrasoundSensor.fetchSample(ultrasoundSample, 0);
 
-        while(Math.abs(sample[0]) >= 0.12f){
-            colorSensor.fetchSample(sample, 0);
+        while (Math.abs(colorSample[0]) >= 0.12f) {
+            colorSensor.fetchSample(colorSample, 0);
             ultrasoundSensor.fetchSample(ultrasoundSample, 0);
             float error = 0.05f - ultrasoundSample[0];
             integral += error;
             derivative = error - previousError;
 
-            pidSpeed(error, derivative, integral, 2000,0,0);
+            pidSpeed(error, derivative, integral, 2000, 0, 0);
 
             Delay.msDelay(10);
         }
 
-        //turnRight();
+        turnRight();
 
         Delay.msDelay(300);
         ultrasoundMotor.rotateTo(0);
 
     }
 
-    private static void turnRight() {
+    private void turnRight() {
         motorRight.setSpeed(180);
         motorLeft.setSpeed(180);
         motorRight.backward();
@@ -129,10 +135,10 @@ public class LineFollow {
         motorRight.stop();
     }
 
-    private static void pidSpeed(float proportional, float derivative, float integral, float kp, float kd, float ki){
-        float speedOffset = kp * proportional + kd * derivative + ki * integral;
-        motorRight.setSpeed((int) (180-speedOffset));
-        motorLeft.setSpeed((int) (180+speedOffset));
+    private void pidSpeed(float proportional, float derivative, float integral, float kp, float kd, float ki) {
+        float turn = kp * proportional + kd * derivative + ki * integral;
+        motorRight.setSpeed((int) (180 - turn));
+        motorLeft.setSpeed((int) (180 + turn));
 
         motorRight.forward();
         motorLeft.forward();
